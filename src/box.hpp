dbd4b67bc951e2b1fe54b2fa0548432c9490de89
7thSamurai <programmer255@pm.me>
2022-05-25 17:30:43 -0400

    Added ability to check if a line is contained in a box

--- src/box.hpp
+++ src/box.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "vec.hpp"
+#include "line.hpp"
 
 template <typename T>
 class Box
@@ -26,8 +27,45 @@ public:
             max_.y = p.y;
     }
 
+    /**
+     * Checks if a point is contained inside
+     * @param p The point to check
+     * @return true if contained
+     */
+    bool contains(const Vec2<T> &p) {
+        if (p.x < min_.x || p.x > max_.x) return false;
+        if (p.y < min_.y || p.y > max_.y) return false;
+
+        return true;
+    }
+
+    /**
+     * Checks if a line is contained inside (At least partially)
+     * @param l The line to check
+     * @return true if contained
+     */
+    bool contains(const Line<T> &l) {
+        // First check if one of the points is contained
+        if (contains(l.a) || contains(l.b))
+            return true;
+
+        // Then check if any of the edges intersect
+        if (l.intersects(Line<T>(top_left (), top_right()))) return true;
+        if (l.intersects(Line<T>(bot_left (), bot_right()))) return true;
+        if (l.intersects(Line<T>(top_left (), bot_left ()))) return true;
+        if (l.intersects(Line<T>(top_right(), bot_right()))) return true;
+
+        return false;
+    }
+
     Vec2<T> min() const { return min_; }
     Vec2<T> max() const { return max_; }
+
+    // Gets the various corners
+    Vec2<T> top_left () const { return Vec2<T>(min_.x, min_.y); }
+    Vec2<T> top_right() const { return Vec2<T>(max_.x, min_.y); }
+    Vec2<T> bot_left () const { return Vec2<T>(min_.x, max_.y); }
+    Vec2<T> bot_right() const { return Vec2<T>(max_.x, max_.y); }
 
     Vec2<T> size() const { return max_ - min_; }
     T width () const { return max_.x - min_.x; }
